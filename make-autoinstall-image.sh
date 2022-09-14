90f0fed6b76985e865d7282acd2a476db4570da2
mtandy <mtandy@mtandy-desktop>
2022-09-14 20:50:12 +0100

    Test playing sound during install

--- make-autoinstall-image.sh
+++ make-autoinstall-image.sh
@@ -32,6 +32,7 @@ if [ ! -f extra-packages/*_amd64.deb ]; then
     mkdir extra-packages
     cd extra-packages
     apt-get download $(apt-rdepends linux-generic|egrep '^(linux-generic|linux-headers-generic|linux-headers|linux-image-|linux-modules-)')
+    apt-get download $(apt-rdepends sox|egrep -v '^ ')
     apt-get download wpasupplicant=2:2.10-6 ppp=2.4.9-1+1ubuntu3 libnl-route-3-200=3.5.0-0.1 pptp-linux=1.10.0-1build3 dnsmasq-base=2.86-1.1 libndp0=1.8-0ubuntu3 libpcsclite1=1.9.5-3 libteamdctl0=1.31-1build2 network-manager=1.36.4-2ubuntu1 network-manager-pptp=1.2.10-1 libbluetooth3=5.64-0ubuntu1 libnm0=1.36.4-2ubuntu1 dns-root-data=2021011101 tpm2-tools libtss2-rc0 libtss2-tctildr0
     cd ..
 fi
@@ -62,6 +63,7 @@ cp install-sources.yaml server-iso-extracted/casper/install-sources.yaml
 cp packages-to-purge.txt server-iso-extracted/packages-to-purge.txt
 cp attempt-wifi-connection.sh server-iso-extracted/
 cp setup-secureboot-mok.sh server-iso-extracted/
+cp /usr/share/sounds/gnome/default/alerts/drip.ogg server-iso-extracted/
 
 mkdir -p server-iso-extracted/extra-packages
 cp extra-packages/* server-iso-extracted/extra-packages/
